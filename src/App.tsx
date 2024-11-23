02237db8bca0774aa56c970933684aad5dd16bf2
Natalya Botsman <botsmannatashaa@gmail.com>
2024-11-23 19:41:25 +0600

    Added routes to switch between pages & displayed Toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import './App.css';
+import Toolbar from './components/Toolbar/Toolbar.tsx';
+import { Route, Routes } from 'react-router-dom';
+import Categories from './containers/Categories/Categories.tsx';
+import Transactions from './containers/Transactions/Transactions.tsx';
 
 const App = () => {
   return (
     <>
-
+      <Toolbar/>
+      <div className="container m-5">
+        <Routes>
+          <Route path="/" element={<Transactions/>}/>
+          <Route path="/home" element={<Transactions/>}/>
+          <Route path="/categories" element={<Categories/>}/>
+        </Routes>
+      </div>
     </>
   );
 };
